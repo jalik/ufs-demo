8a75ee92d9d358c22e3544ef77fdbc15154c8433
jalik <karl.stein.pro@gmail.com>
2016-09-21 12:58:13 -1000

    Adds example of using "UploadFS.addMimeType()" Upgrades UploadFS package to v0.6.9

--- imports/server/index.js
+++ imports/server/index.js
@@ -7,3 +7,7 @@ import './publications';
 // Load stores
 import './stores/thumbnails';
 import './stores/files';
+
+// Configure UploadFS
+UploadFS.addMimeType('kml', 'application/vnd.google-earth.kml+xml');
+UploadFS.addMimeType('kmz', 'application/vnd.google-earth.kmz');
