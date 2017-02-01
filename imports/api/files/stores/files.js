874992b9459b08307bc912df207874b9cd4c8766
jalik <karl.stein.pro@gmail.com>
2017-01-31 17:58:21 -1000

    Adds license on each file Updates UploadFS packages

--- imports/api/files/stores/files.js
+++ imports/api/files/stores/files.js
@@ -1,3 +1,28 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2017 Karl STEIN
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
 import gm from 'gm';
 import {UploadFS} from 'meteor/jalik:ufs';
 import {Files} from '../collections/files';
@@ -27,7 +52,6 @@ export const FileStore = new UploadFS.store.Local({
     // Overwrite default permissions
     permissions: new UploadFS.StorePermissions({
         insert(userId, file) {
-            console.log(userId, file.userId);
             // allow anyone to upload a file, but check that uploaded file is attached to the user that uploads the file
             return !file.userId || file.userId === userId;
         },
